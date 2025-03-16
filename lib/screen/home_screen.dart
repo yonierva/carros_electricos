e7484f0070408bc0e7c3e3388e3f33356519980d
yonierva <yoniervasquez98@gmail.com>
2025-03-15 19:59:20 -0500

    inicio de sesion

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,68 +1,80 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'scan_qr_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   HomeScreen({super.key});
-
-  final String usuario = "Pepe Perez";
-  final List<Map<String, String>> carros = [
-    {"placa": "ERF888", "conductor": "Juan Carlos", "empresa": "XYZ"},
-    {"placa": "ABC123", "conductor": "Maria López", "empresa": "XYZ"},
-    {"placa": "XYZ789", "conductor": "Pedro Gómez", "empresa": "XYZ"},
-  ];
+  
+  final TextEditingController user = TextEditingController();
+  final TextEditingController password = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("Carros Eléctricos")),
-      body: Column(
-        children: [
-          Container(
-            padding: const EdgeInsets.all(16),
-            color: Colors.grey[300],
-            width: double.infinity,
-            child: Text(
-              "Usuario: $usuario",
-              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              textAlign: TextAlign.center,
-            ),
-          ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: carros.length,
-              itemBuilder: (context, index) {
-                final carro = carros[index];
-                return Card(
-                  margin: const EdgeInsets.all(10),
-                  child: ListTile(
-                    leading: Container(
-                      width: 50,
-                      height: 50,
-                      color: Colors.grey[300],
-                      child: const Center(child: Text("IMG")),
-                    ),
-                    title: Text("Placa: ${carro['placa']}"),
-                    subtitle: Text(
-                      "Conductor: ${carro['conductor']}\nEmpresa: ${carro['empresa']}",
-                    ),
-                    trailing: ElevatedButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => const ScanQRScreen(),
-                          ),
-                        );
-                      },
-                      child: const Text("Escanear QR"),
-                    ),
+    return GestureDetector(
+      onTap: () {
+      FocusScope.of(context).unfocus(); // Oculta el teclado
+    },child: Scaffold(
+      resizeToAvoidBottomInset: false, // Evita que la UI se reacomode lentamente
+      appBar: AppBar(title: const Text("Inicio de Sesion", style: TextStyle(color: Colors.blue, fontSize: 30 )), centerTitle: true),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        child: Form(
+          child: Center(
+            child: Column(
+              children: [
+                TextFormField(
+                  controller: user,
+                  decoration: InputDecoration(
+                    labelText: "Usuario",
+                    hintText: "Ingrese su nombre",
+                    icon: Icon(Icons.person)
                   ),
-                );
-              },
-            ),
-          ),
-        ],
-      ),
+                ),
+                TextFormField(
+                  controller: password,
+                  decoration: InputDecoration(
+                    labelText: "Contraseña",
+                    floatingLabelBehavior: FloatingLabelBehavior.auto,
+                    hintText: "Ingrese una contraseña",
+                    icon: Icon(Icons.lock)
+                  ),
+                  obscureText: true,
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: Size(double.infinity, 50),
+                    backgroundColor: Colors.blue,
+                    foregroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20)
+                    ),
+                    elevation: 10,
+                  ),
+                  onPressed: () {
+                    if(user.text.isNotEmpty && password.text.isNotEmpty){
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => ScanQRScreen()));
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text('bienvenido ${user.text}')
+                      ),
+                    );
+                    }
+                    else{
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text('Por favor llene los campos'), duration: Duration(seconds: 2),)
+                      );
+                    }
+                  },
+                  child: Text("Iniciar Sesion"),
+                )
+              ],
+            ) 
+          )
+        )
+      )
+    )
     );
-  }
+  }              
 }
+
